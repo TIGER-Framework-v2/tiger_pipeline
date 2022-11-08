ab3b62ccaf5842bfac1b85d90a6c2f3cad2f7edb
Dzmitry Dzerban <dzmitry_dzerban@epam.com>
2022-11-08 20:24:20 +0300

    Creating test bucket

--- tf/main.tf
+++ tf/main.tf
@@ -16,3 +16,16 @@ provider "aws" {
   region  = "us-east-2"
 }
 
+resource "aws_s3_bucket" "tf_test_bucket" {
+  bucket = var.test_bucket_name
+
+  tags = {
+    Name        = "tf_test_bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "tf_test_bucket_acl" {
+  bucket = aws_s3_bucket.tf_test_bucket.id
+  acl    = "private"
+}
